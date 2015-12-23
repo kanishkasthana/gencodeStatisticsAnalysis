e929ee9167c8a5a57bc7e1bf212d6d3ec1006ab1
kanishk <kani.asthana@gmail.com>
2015-12-23 00:50:40 -0800

    This does not seem to be working I will not longer commit to this project, this is failed code

--- analysis.R
+++ analysis.R
@@ -4,6 +4,12 @@ allFileNames=list.files(pattern="*.dat")
 allData=sapply(allFileNames, function(fileName){read.csv(file=fileName,header=TRUE, sep="\t")})
 
 #Taking a subset of data which is in the standard tabular format
-uniformDataSubset=allData[c(-16,-17)]
+uniformDataSubset=allData[c(-20,-21)]
+version3d=allData[20]
+version3c=allData[21]
 
-uniformDataSubset=sapply(uniformDataSubset,function(data){data})
+totalGenesFromUniformData=sapply(uniformDataSubset,function(data){
+  return(data$genes[data$biotype=="protein_coding"])
+})
+
+data=as.data.frame(allData[1])
